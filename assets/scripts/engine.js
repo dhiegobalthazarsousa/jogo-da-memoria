f896ee345f97a521bfb211d24a7fbd000ecaf230
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2023-12-31 00:39:43 -0300

    feat: add cards dinamicamente

---
+++ assets/scripts/engine.js
@@ -0,0 +1,25 @@
+const emojis = [
+    "😂",
+    "😂",
+    "😍",
+    "😍",
+    "🤖",
+    "🤖",
+    "👽",
+    "👽",
+    "🐉",
+    "🐉",
+    "👹",
+    "👹",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5) ? 2 : -1);
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.classList.add("item");
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
