80ff1a31cf041d4b69889eb150bde06ebe428d71
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2024-01-07 18:57:18 -0300

    feat: girando as cartas ao clicar, fix-bug: não clica na mesma carta

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -17,9 +17,27 @@ let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5) ? 2 : -1);
 
+
+
+
 for(let i=0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.classList.add("item");
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2 && !this.className.includes("boxOpen")){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if(openCards.length === 2){
+        setTimeout(checkMatches, 500);
+    }
+}
+
+function checkMatches(){
+    //todo
 }
